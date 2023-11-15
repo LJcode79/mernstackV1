eb11be0a44491f404150b6ca6dea466166288ea2
LJ <ljdb1999@gmail.com>
2023-11-15 07:13:38 -0800

    vercel V12

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,12 @@ const userRoutes = require('./routes/user')
 const app = express()
 
 //middleware
-app.use(express.json())
+// app.use(express.json())
 
-app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+// app.use((req, res, next) => {
+//     console.log(req.path, req.method)
+//     next()
+// })
 
 //routes
 app.get('/', (req, res) => {
